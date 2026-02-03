928e61cf043f219bf5b62ac9f5670eb57e179526
Ashutosh <ashubhar574@gmail.com>
2026-02-03 17:49:14 +0530

    Create cosmos_ouput.py

---
+++ cosmos_ouput.py
@@ -0,0 +1,351 @@
+{
+    "id": "FN3TXJEEXPC9RRT6WUPTWG37X",
+    "tdnakey": "FN3TXJEEXPC9RRT6WUPTWG37X",
+    "profile_version": "1.0",
+    "context_datetime": "2026-01-30T18:08:53Z",
+    "msg_type": "TDNA",
+    "context_process": "CustomerCreate",
+    "name": "MR ORIOL TEVA SANCHEZ",
+    "contact_details": {
+        "emails": [
+            {
+                "email_address": "ALBANEGROROCA@GMAIL.COM",
+                "source_id": "CEM_DERIVED_DATA-1",
+                "source_type": "CEM_DERIVED_DATA"
+            }
+        ],
+        "phones": [
+            {
+                "phone_number": "656625858",
+                "source_id": "CEM_DERIVED_DATA-1",
+                "source_type": "CEM_DERIVED_DATA"
+            }
+        ],
+        "addresses": []
+    },
+    "demographics": [
+        {
+            "birth_date": null,
+            "birth_country": null,
+            "birth_place": null,
+            "nationality": null,
+            "gender": "MALE",
+            "civil_status": null,
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        }
+    ],
+    "identity_documents": [],
+    "metrics": [
+        {
+            "code": "AASR",
+            "name": "Average Ancillary Services Revenue",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "AIRCRAFTNF",
+            "name": "Aircraft Type On Next Flight",
+            "value": "789",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "ANFMCY",
+            "name": "Average Number of Flights per Month - Current Year",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "ANFMLY",
+            "name": "Average Number of Flights per Month - Last Year",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "ANJMCY",
+            "name": "Average Number of Journeys per Month - Current Year",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "ANJMLY",
+            "name": "Average Number of Journeys per Month - Last Year",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "BAGNF",
+            "name": "Extra Baggage On Next Flight",
+            "value": "FALSE",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "BP12M",
+            "name": "PNRs Booked Last Twelve Months",
+            "value": "1",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "BPCY",
+            "name": "PNRs Booked Current Year",
+            "value": "1",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "BPL",
+            "name": "PNRs Booked - Lifetime",
+            "value": "1",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "CGEM",
+            "name": "Consent Granted on E-mail for Marketing",
+            "value": "FALSE",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "CGEP",
+            "name": "Consent Granted on E-mail for Personalization",
+            "value": "FALSE",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "DTNF",
+            "name": "Days to next flight",
+            "value": "24",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "FFL",
+            "name": "Nb of flights Flown lifetime",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "FFL12M",
+            "name": "Flights Flown Last Twelve Months",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "FFL1M",
+            "name": "Flights Flown Last Month",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "FFL6M",
+            "name": "Flights Flown Last Six Months",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "FFLCY",
+            "name": "Flights Flown Current Year",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "FFLLY",
+            "name": "Flights Flown Last Year",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "HFF",
+            "name": "Has a Flight in the Future",
+            "value": "TRUE",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "LBD",
+            "name": "Last booking date",
+            "value": "2026-01-30T18:08:00Z",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "PDES",
+            "name": "Preferred Destination",
+            "value": "CMB",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "PDESA",
+            "name": "Preferred Destination Airport",
+            "value": "CMB",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "PLANG",
+            "name": "Preferred Language",
+            "value": "ES",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "PORI",
+            "name": "Preferred Origin",
+            "value": "BCN",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "PORIA",
+            "name": "Preferred Origin Airport",
+            "value": "BCN",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "SEATNF",
+            "name": "Chargeable Seat On Next Flight",
+            "value": "FALSE",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "TASR",
+            "name": "Total Ancillary Services Revenue",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "TASR12M",
+            "name": "Total ancillary revenue Last Twelve Months",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "TFR",
+            "name": "Total Flight Revenue",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "VIP",
+            "name": "VIP",
+            "value": "FALSE",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "VUPGR",
+            "name": "Voluntary Upgraded Flights Last Six Months",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "ABR",
+            "name": "Average Booking Revenue",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "ANPM12M",
+            "name": "Average Number of PNRs per Month - Last Twelve Months",
+            "value": "0.08",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "ANPMCY",
+            "name": "Average Number of PNRs per Month - Current Year",
+            "value": "1",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "PBCH",
+            "name": "Preferred Booking Channel",
+            "value": "ONLINE_WEBSITE",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "RFMF",
+            "name": "RFM - Frequency",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "RFMR",
+            "name": "RFM - Recency",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "TRV",
+            "name": "Total Revenue",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "RFMM",
+            "name": "RFM - Monetary",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        },
+        {
+            "code": "RFM",
+            "name": "RFM Value",
+            "value": "0",
+            "source_id": "CEM_DERIVED_DATA-1",
+            "source_type": "CEM_DERIVED_DATA"
+        }
+    ],
+    "memberships": [],
+    "dictionary": [
+        {
+            "ticketing_last_modified_datetime": null,
+            "ticketing_received_datetime": null,
+            "ticket_id": null,
+            "ticket_number": null,
+            "pnr_creation_datetime": "2026-01-30T18:08:00Z",
+            "pnr_last_modified_datetime": "2026-01-30T18:08:37Z",
+            "pnr_link_id": "PNR-2",
+            "number_in_party": 2,
+            "passenger_tattoo": 3,
+            "pnr_number": "8SNPNV",
+            "traveler_ids": "8SNPNV-2026-01-30-PT-3"
+        }
+    ],
+    "enqueued_datetime": "2026-01-30T18:08:53Z",
+    "cosmos_processed_datetime": "2026-02-01T12:41:23Z",
+    "_rid": "LsY1ANVrBzOG-C8AAAAAAA==",
+    "_self": "dbs/LsY1AA==/colls/LsY1ANVrBzM=/docs/LsY1ANVrBzOG-C8AAAAAAA==/",
+    "_etag": "\"9700a480-0000-3200-0000-697f4a1a0000\"",
+    "_attachments": "attachments/",
+    "_ts": 1769949722
+}
